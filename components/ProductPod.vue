2c06485fc94044b81a0601a144edce629024ffb0
Asieef <asieef.chowdhury@gmail.com>
2023-12-23 13:30:10 +0600

    product pod

---
+++ components/ProductPod.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="group relative" v-for="product in data.data">
+    <div
+      class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80"
+    >
+      <img
+        :src="product.thumbnail"
+        alt="Front of men&#039;s Basic Tee in black."
+        class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+      />
+    </div>
+    <div class="mt-4 flex justify-between">
+      <div>
+        <h3 class="text-sm text-gray-700">
+          <a href="#">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            {{ product.name }}
+          </a>
+        </h3>
+        <p class="mt-1 text-sm text-gray-500">Black</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps([data]);
+
+console.log(props.data);
+</script>
