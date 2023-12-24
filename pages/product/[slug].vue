2c9077b39c14e89cdacb9be7ce766a8185de987c
masumcity <riaz.masum@comcitybd.com>
2023-12-24 13:22:06 +0600

    fix product tab

--- pages/product/[slug].vue
+++ pages/product/[slug].vue
@@ -3,53 +3,40 @@
   <div class="container mx-auto pt-24">
     <ol class="flex items-center whitespace-nowrap" aria-label="Breadcrumb">
       <li class="inline-flex items-center">
-        <a
-          class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
-          href="#"
-        >
+        <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
+          href="#">
           Home
         </a>
-        <svg
-          class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
+        <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600"
+          xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+          stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="m9 18 6-6-6-6" />
         </svg>
       </li>
       <li class="inline-flex items-center">
-        <a
-          class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
-          href="#"
-        >
+        <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
+          href="#">
           Product
-          <svg
-            class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
+          <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600"
+            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="m9 18 6-6-6-6" />
           </svg>
         </a>
       </li>
-      <li
-        class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200"
-        aria-current="page"
-      >
+      <li class="inline-flex items-center">
+        <NuxtLink :to="`/category/${data.category.slug}`"
+          class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500">
+          {{ data.category.name }}
+          <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600"
+            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="m9 18 6-6-6-6" />
+          </svg>
+        </NuxtLink>
+      </li>
+      <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200"
+        aria-current="page">
         {{ data.name }}
       </li>
     </ol>
@@ -62,23 +49,13 @@
         <div class="w-full px-4 md:w-1/2">
           <div class="sticky top-0 z-50 overflow-hidden">
             <div class="relative mb-6 lg:mb-10 lg:h-2/4">
-              <img
-                :src="data.photo"
-                alt=""
-                class="object-cover w-full lg:h-full"
-              />
+              <img :src="data.photo" alt="" class="object-cover w-full lg:h-full" />
             </div>
             <div class="flex-wrap hidden md:flex">
               <div class="w-1/2 p-2 sm:w-1/4" v-for="photo in data.gallery">
-                <a
-                  href="#"
-                  class="block border border-red-300 dark:border-transparent dark:hover:border-red-300 hover:border-red-300"
-                >
-                  <img
-                    :src="photo.src"
-                    alt=""
-                    class="object-cover w-full lg:h-20"
-                  />
+                <a href="#"
+                  class="block border border-red-300 dark:border-transparent dark:hover:border-red-300 hover:border-red-300">
+                  <img :src="photo.src" alt="" class="object-cover w-full lg:h-20" />
                 </a>
               </div>
             </div>
@@ -87,27 +64,18 @@
         <div class="w-full px-4 md:w-1/2">
           <div class="lg:pl-20">
             <div class="mb-8">
-              <span
-                class="text-lg font-medium text-rose-500 dark:text-rose-200"
-                >{{ data.model }}</span
-              >
-              <h2
-                class="max-w-xl mt-2 mb-6 text-2xl font-bold dark:text-gray-400 md:text-4xl"
-              >
+              <span class="text-lg font-medium text-rose-500 dark:text-rose-200">{{ data.model }}</span>
+              <h2 class="max-w-xl mt-2 mb-6 text-2xl font-bold dark:text-gray-400 md:text-4xl">
                 {{ data.name }}
               </h2>
 
-              <div
-                class="max-w-md mb-8 text-gray-700 dark:text-gray-400"
-                v-html="data.short"
-              ></div>
+              <div class="max-w-md mb-8 text-gray-700 dark:text-gray-400" v-html="data.short"></div>
             </div>
 
             <div class="flex flex-wrap items-center -mx-4">
               <div class="w-full px-4 mb-4 lg:w-1/2 lg:mb-0">
                 <button
-                  class="flex items-center justify-center w-full p-4 text-red-500 border border-red-500 rounded-md dark:text-gray-200 dark:border-red-600 hover:bg-red-600 hover:border-red-600 hover:text-gray-100 dark:bg-red-600 dark:hover:bg-red-700 dark:hover:border-red-700 dark:hover:text-gray-300"
-                >
+                  class="flex items-center justify-center w-full p-4 text-red-500 border border-red-500 rounded-md dark:text-gray-200 dark:border-red-600 hover:bg-red-600 hover:border-red-600 hover:text-gray-100 dark:bg-red-600 dark:hover:bg-red-700 dark:hover:border-red-700 dark:hover:text-gray-300">
                   Buy Now
                 </button>
               </div>
@@ -119,133 +87,7 @@
   </section>
 
   <!-- Spec Tabs -->
-  <div class="pb-16">
-    <div
-      class="mb-4 border-b border-gray-200 dark:border-gray-700 container mx-auto"
-    >
-      <ul
-        class="flex flex-wrap -mb-px text-sm font-medium text-center"
-        id="default-tab"
-        data-tabs-toggle="#default-tab-content"
-        role="tablist"
-      >
-        <li class="me-2" role="presentation">
-          <button
-            class="inline-block p-4 border-b-2 rounded-t-lg"
-            id="profile-tab"
-            data-tabs-target="#profile"
-            type="button"
-            role="tab"
-            aria-controls="profile"
-            aria-selected="false"
-          >
-            Description
-          </button>
-        </li>
-        <li class="me-2" role="presentation">
-          <button
-            class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            id="dashboard-tab"
-            data-tabs-target="#dashboard"
-            type="button"
-            role="tab"
-            aria-controls="dashboard"
-            aria-selected="false"
-          >
-            Video
-          </button>
-        </li>
-        <li class="me-2" role="presentation">
-          <button
-            class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            id="settings-tab"
-            data-tabs-target="#settings"
-            type="button"
-            role="tab"
-            aria-controls="settings"
-            aria-selected="false"
-          >
-            Download
-          </button>
-        </li>
-        <li role="presentation">
-          <button
-            class="inline-block p-4 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-            id="contacts-tab"
-            data-tabs-target="#contacts"
-            type="button"
-            role="tab"
-            aria-controls="contacts"
-            aria-selected="false"
-          >
-            Specification
-          </button>
-        </li>
-      </ul>
-    </div>
-    <div id="default-tab-content">
-      <div
-        class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800 container mx-auto"
-        id="profile"
-        role="tabpanel"
-        aria-labelledby="profile-tab"
-      >
-        <p class="text-sm text-gray-500 dark:text-gray-400">
-          This is some placeholder content the
-          <strong class="font-medium text-gray-800 dark:text-white"
-            >Profile tab's associated content</strong
-          >. Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </p>
-      </div>
-      <div
-        class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800 container mx-auto"
-        id="dashboard"
-        role="tabpanel"
-        aria-labelledby="dashboard-tab"
-      >
-        <p class="text-sm text-gray-500 dark:text-gray-400">
-          This is some placeholder content the
-          <strong class="font-medium text-gray-800 dark:text-white"
-            >Dashboard tab's associated content</strong
-          >. Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </p>
-      </div>
-      <div
-        class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800 container mx-auto"
-        id="settings"
-        role="tabpanel"
-        aria-labelledby="settings-tab"
-      >
-        <p class="text-sm text-gray-500 dark:text-gray-400">
-          This is some placeholder content the
-          <strong class="font-medium text-gray-800 dark:text-white"
-            >Settings tab's associated content</strong
-          >. Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </p>
-      </div>
-      <div
-        class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800 container mx-auto"
-        id="contacts"
-        role="tabpanel"
-        aria-labelledby="contacts-tab"
-      >
-        <p class="text-sm text-gray-500 dark:text-gray-400">
-          This is some placeholder content the
-          <strong class="font-medium text-gray-800 dark:text-white"
-            >Contacts tab's associated content</strong
-          >. Clicking another tab will toggle the visibility of this one for the
-          next. The tab JavaScript swaps classes to control the content
-          visibility and styling.
-        </p>
-      </div>
-    </div>
-  </div>
+  <Tab />
 </template>
 
 <script setup>
