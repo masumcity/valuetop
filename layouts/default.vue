8f543a480a114851a8d10f7fbc25d74f7e6417e0
masumcity <riaz.masum@comcitybd.com>
2025-02-12 11:33:40 +0600

    tianji

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,3 +4,15 @@
   <NuxtPage />
   <Footer />
 </template>
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const script = document.createElement('script');
+  script.async = true;
+  script.defer = true;
+  script.src = 'https://tianji.comcitybd.net/tracker.js';
+  script.dataset.websiteId = 'cm71gzg5c311savk8poskqtun';
+  document.head.appendChild(script);
+});
+</script>
